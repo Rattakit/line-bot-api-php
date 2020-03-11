b1e67aa3fa3bd9d4943aedd839eda23febd4f5ff
Rattakit <55879598+Rattakit@users.noreply.github.com>
2020-03-11 19:51:30 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'BvDsy8t/wkYCho4aIR/uK1JJquwnnTI7vnenWiu3Df6TEvLoBptYR7HonAtmHUKDQOffAkWetc4iw4Mkw7u4rlMM/VsBB6A5D7DfZ3NjO9kwWKC6wiHm3vvk86eBVCKUyT1nwxoYwLl0ok5n1JxrhQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'bPHElS7roUPJ9cniT64p0P44VgFAwZuyNIY2+2NZ3pgtLCh5/pq3pW/gLex4Nlx3ASYwh9MRqzIBuc7enKv556vJmTVzn/ZP3ensuAbmXjeV/hGW9OUi7MoCzNV5zfGYMDfFcY2ceJdQiqc9roS6NgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
